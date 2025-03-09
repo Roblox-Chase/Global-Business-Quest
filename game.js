039f09cd2c06bddf433a0840ec3cfe2532da5557
CodeCrafterX <Himashuparikh54@gmail.com>
2025-03-08 19:27:15 -0500

    Update game.js

--- game.js
+++ game.js
@@ -9,7 +9,7 @@ import { ScenarioManager, additionalScenarios } from './js/scenarios.js';
 import { UIManager } from './js/ui.js';
 import { TokyoOfficeEnvironment } from './js/countries/TokyoOfficeEnvironment.js';
 import { ParisRestaurantEnvironment } from './js/countries/ParisRestaurantEnvironment.js';
-import { textureManager } from './utils/textures.js';
+import { textureManager } from './assets/textures.js';
 
 // Main Game Class
 class GlobalBusinessQuest {
